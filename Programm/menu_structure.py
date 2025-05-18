8af37eab0f46526f9c49618bbe8f4bb46b2adaf7
Cioways <88454328+Cioways@users.noreply.github.com>
2025-05-18 12:48:17 +0200

    Add files via upload

---
+++ Programm/menu_structure.py
@@ -0,0 +1,17 @@
+menu = {
+    "WLAN": {
+        "Nmap Scan": "views.nmap_view.NmapView",
+        "Airodump-ng": "views.airodump_view.AirodumpView",
+        "Reaver WPS": "views.reaver_view.ReaverView",
+        "Wifi Scanner": "views.wifi_scan_view.WifiScanView",
+        "Zurück": "BACK"
+    },
+    "Bluetooth": {
+        "Scan": "views.bt_scan_view.BluetoothScanView",
+        "Zurück": "BACK"
+    },
+    "Allgemein": {
+        "Über System": "views.sysinfo_view.SysInfoView",
+        "Zurück": "BACK"
+    }
+}
---
+++ Programm/theme.py
@@ -0,0 +1,7 @@
+# theme.py
+BG_COLOR = "#0c0f14"
+BTN_COLOR = "#1f2a44"
+BTN_HOVER = "#2f3e5a"
+TEXT_COLOR = "#dcdcdc"
+TEXT_HIGHLIGHT = "#00ffaa"
+FONT = ("Arial", 12, "bold")
